1601575f0258591f33539d6aafe64b91764e5acb
Mark Ian Holland <markianholland@live.co.uk>
2019-07-01 17:25:16 +0100

    [libmorton] update to 0.2

--- ports/libmorton/portfile.cmake
+++ ports/libmorton/portfile.cmake
@@ -3,13 +3,12 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Forceflow/libmorton
-    REF 797ea736dca49553a56089f143ee6d1effdd318e
-    SHA512 ee9632f5c873462842d18014d4fd2d461e9fe767659e7426a7dec90fcc06cb974fb064229db5622c38ad0af9509004edea87e0f1d57ad09d8d1d236a5b9579a0
+    REF v0.2
+    SHA512 04cf8332a1cee52eebfb25a46ac64517900704f62fe53ccf1d6a74c535ccac5af4e3ce2e0a5ce94ee850fadb429fe0d88d5a66901f16e4308341a621e599d33d
     HEAD_REF master
 )
 
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libmorton)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/libmorton/LICENSE ${CURRENT_PACKAGES_DIR}/share/libmorton/copyright)
+configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/libmorton/copyright COPYONLY)
 
 file(GLOB HEADER_FILES ${SOURCE_PATH}/libmorton/include/*.h)
 file(COPY ${HEADER_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libmorton)
